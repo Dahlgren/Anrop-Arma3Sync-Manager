fcae363a23f0e411646d4777bb4e257bc96a96e5
Björn Dahlgren <bjorn@dahlgren.at>
2016-10-24 11:33:54 +0200

    Update outdated mods

--- app/mods/mods.ctrl.js
+++ app/mods/mods.ctrl.js
@@ -1,11 +1,16 @@
 angular.module('app')
-.controller('ModsCtrl', function ($scope, ModsSvc) {
+.controller('ModsCtrl', function ($scope, ModsSvc, PlayWithSixSvc) {
   $scope.$on('socket:mods', function (ev, data) {
     $scope.mods = data;
   });
 
   $scope.delete = function (mod) {
     ModsSvc.delete(mod.name).then(function () {
+    });
+  };
+
+  $scope.update = function (mod) {
+    PlayWithSixSvc.download(mod.name).then(function () {
     });
   };
 });
