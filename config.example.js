30b8895ab8587523a2a8002519ef52dcee2d2112
Björn Dahlgren <bjorn@dahlgren.at>
2016-10-27 00:11:08 +0200

    Define arma3sync jar file path in config

--- config.example.js
+++ config.example.js
@@ -1,5 +1,6 @@
 module.exports = {
   arma3sync: '/path/to/arma3sync/data/directory',
+  arma3syncJarFile: '/path/to/arma3sync.jar',
   java: '/path/to/java/binary',
   path: '/path/to/arma3sync/output/directory',
   port: 3000,
