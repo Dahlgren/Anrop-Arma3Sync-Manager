d15013072e86c32d57cf5ad605547963d03c0aec
Björn Dahlgren <bjorn@dahlgren.at>
2017-07-05 15:55:57 +0200

    Calculate dependencies

--- app/mods/mods.ctrl.js
+++ app/mods/mods.ctrl.js
@@ -5,6 +5,66 @@ angular.module('app')
     $scope.downloadButtonDisabled = disabled
     $scope.refreshButtonDisabled = disabled
   }
+
+  var showConfirmDeleteDialog = function (mod) {
+    SweetAlert.swal(
+      {
+        title: 'Are you sure?',
+        text: 'You will delete <strong>' + mod.name + '</strong> from the server',
+        type: 'warning',
+        confirmButtonColor: '#DD6B55',
+        confirmButtonText: 'Yes, delete it!',
+        html: true,
+        showCancelButton: true,
+        closeOnConfirm: false,
+        showLoaderOnConfirm: true
+      },
+      function (isConfirm) {
+        if (isConfirm) {
+          ModsSvc.delete(mod.name).then(function () {
+            showDeletedDialog(mod)
+          })
+        }
+      }
+    )
+  }
+
+  var showRequiredDialog = function (mod) {
+    var requiredBy = mod.playWithSix.requiredBy.map(function (mod) {
+      return '<strong>' + mod.name + '</strong>'
+    }).join(', ')
+
+    SweetAlert.swal(
+      {
+        title: 'Are you sure?',
+        text: 'You will delete <strong>' + mod.name + '</strong> from the server. It\'s required by ' + requiredBy,
+        type: 'error',
+        confirmButtonColor: '#DD6B55',
+        confirmButtonText: 'Yes, delete it!',
+        html: true,
+        showCancelButton: true,
+        closeOnConfirm: false,
+        showLoaderOnConfirm: true
+      },
+      function (isConfirm) {
+        if (isConfirm) {
+          ModsSvc.delete(mod.name).then(function () {
+            showDeletedDialog(mod)
+          })
+        }
+      }
+    )
+  }
+
+  var showDeletedDialog = function (mod) {
+    SweetAlert.swal({
+      title: 'Deleted!',
+      text: '<strong>' + mod.name + '</strong> has been deleted',
+      type: 'success',
+      html: true
+    })
+  }
+
   setDisabledState(true)
   $scope.refreshButtonAnimated = false
 
@@ -18,31 +78,11 @@ angular.module('app')
   })
 
   $scope.delete = function (mod) {
-    SweetAlert.swal(
-      {
-        title: 'Are you sure?',
-        text: 'You will delete <strong>' + mod.name + '</strong> from the server',
-        type: 'warning',
-        confirmButtonColor: '#DD6B55',
-        confirmButtonText: 'Yes, delete it!',
-        html: true,
-        showCancelButton: true,
-        closeOnConfirm: false,
-        showLoaderOnConfirm: true
-      },
-     function (isConfirm) {
-       if (isConfirm) {
-         ModsSvc.delete(mod.name).then(function () {
-           SweetAlert.swal({
-             title: 'Deleted!',
-             text: '<strong>' + mod.name + '</strong> has been deleted',
-             type: 'success',
-             html: true
-           })
-         })
-       }
-     }
-    )
+    if (mod.playWithSix && mod.playWithSix.requiredBy && mod.playWithSix.requiredBy.length > 0) {
+      showRequiredDialog(mod)
+    } else {
+      showConfirmDeleteDialog(mod)
+    }
   }
 
   $scope.refresh = function () {
@@ -53,5 +93,33 @@ angular.module('app')
   $scope.update = function (mod) {
     PlayWithSixSvc.download(mod.name).then(function () {
     })
+  }
+
+  $scope.hasAllRequired = function (mod) {
+    if (!mod.playWithSix) {
+      return true
+    }
+
+    var missingMods = mod.playWithSix.requires.filter(function (requiredMod) {
+      return !requiredMod.exists
+    })
+
+    return missingMods.length === 0
+  }
+
+  $scope.showModsPopup = function (title, subtitle, mods) {
+    var names = mods.map(function (mod) {
+      return mod.name
+    }).join(', ')
+
+    SweetAlert.swal(
+      {
+        title: title,
+        text: '<strong>' + subtitle + '</strong><br/>' + names,
+        type: 'info',
+        html: true,
+        closeOnConfirm: true
+      }
+    )
   }
 })
