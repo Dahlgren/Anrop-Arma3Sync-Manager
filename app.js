70abce0bfc450d41e872f97e406434216097792c
Björn Dahlgren <bjorn@dahlgren.io>
2021-08-09 02:55:04 +0200

    Steam Workshop validate files

--- app.js
+++ app.js
@@ -11,11 +11,13 @@ const http = require('http')
 const logger = require('morgan')
 const path = require('path')
 const SocketIO = require('socket.io').Server
+const SteamUser = require('steam-user')
 
 const State = require('./lib/State')
 const Arma3Sync = require('./lib/Arma3Sync')
 const FileHashCache = require('./lib/FileHashCache')
 const Mods = require('./lib/Mods')
+const SteamWorkshop = require('./lib/SteamWorkshop')
 
 const app = express()
 const server = http.Server(app)
@@ -28,13 +30,38 @@ app.use(require('connect-livereload')())
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, 'public')))
 
-const state = new State()
 const fileHashCache = new FileHashCache(
   process.env.ARMA3SYNC_REPO_PATH,
   process.env.FILE_HASH_CACHE || 'hash-cache.json'
 )
+
+const steamUser = new SteamUser()
+const steamWorkshop = new SteamWorkshop(steamUser, fileHashCache)
+
+const state = new State()
 const arma3sync = new Arma3Sync(state, fileHashCache)
-const mods = new Mods(state, arma3sync)
+const mods = new Mods(state, arma3sync, steamUser, steamWorkshop, fileHashCache)
+
+if (process.env.STEAM_USERNAME && process.env.STEAM_PASSWORD) {
+  steamUser.logOn({
+    accountName: process.env.STEAM_USERNAME,
+    password: process.env.STEAM_PASSWORD
+  })
+
+  steamUser.on('loggedOn', (details) => {
+    console.log('Logged onto Steam as ' + steamUser.steamID.steam3())
+  })
+
+  steamUser.on('contentServersReady', (details) => {
+    mods.contentServersReady = true
+
+    if (!state.refreshing) {
+      mods.updateMods()
+    }
+  })
+} else {
+  mods.updateMods()
+}
 
 app.use('/api', require('./api')(arma3sync, mods))
 
