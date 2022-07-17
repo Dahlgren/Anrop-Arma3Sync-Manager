566513ee96f595fd5904ee06e9149982d6edf111
Björn Dahlgren <bjorn@dahlgren.io>
2022-07-17 15:54:45 +0200

    Migrate to arma3sync-lib instead of java based arma3sync

--- app/arma3sync/arma3sync.ctrl.js
+++ app/arma3sync/arma3sync.ctrl.js
@@ -2,16 +2,25 @@ angular.module('app')
   .controller('Arma3SyncCtrl', function ($scope, Arma3SyncSvc) {
     const setDisabledState = (disabled) => {
       $scope.buildButtonDisabled = disabled
+      $scope.initButtonDisabled = disabled
     }
     setDisabledState(true)
     $scope.buildButtonAnimated = false
+    $scope.initButtonAnimated = false
 
     $scope.$on('socket:state', (ev, state) => {
       $scope.buildButtonAnimated = state.building
+      $scope.buildButtonVisible = state.initialized
+      $scope.initButtonAnimated = state.building
+      $scope.initButtonVisible = !state.initialized
       setDisabledState(state.building || state.downloading || state.refreshing)
     })
 
     $scope.build = () => {
       Arma3SyncSvc.build()
+    }
+
+    $scope.init = () => {
+      Arma3SyncSvc.init()
     }
   })
--- config.example.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  arma3sync: '/path/to/arma3sync/data/directory',
-  arma3syncJarFile: '/path/to/arma3sync.jar',
-  java: '/path/to/java/binary',
-  path: '/path/to/arma3sync/output/directory',
-  port: 3000,
-  host: '0.0.0.0', // Can be either an IP or a Hostname
-  repository: 'repository_name'
-}
